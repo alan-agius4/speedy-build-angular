1d4c6d96a3fce3a45767fe5e3cdcdd082e6264d7
Alan Agius <alan.agius@betssongroup.com>
2019-06-17 10:29:51 +0200

    fix: update to architect api to version 8
    Closes #25

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,12 @@
 
-import {
-	Builder,
-	BuilderConfiguration,
-	BuilderContext,
-	BuildEvent
-} from "@angular-devkit/architect";
-import { getSystemPath } from "@angular-devkit/core";
+import { BuilderContext, BuilderOutput, createBuilder } from "@angular-devkit/architect";
+import { json } from "@angular-devkit/core";
 import { readFileSync } from "fs";
 import * as glob from "glob";
 import * as path from "path";
-import { from, Observable } from "rxjs";
-import { concatMap } from "rxjs/operators";
 import * as stylelint from "stylelint";
 
-export interface StylelintBuilderOptions {
+export interface StylelintBuilderOptions extends json.JsonObject {
 	stylelintConfig: string;
 	fix: boolean;
 	force: boolean;
@@ -23,62 +16,53 @@ export interface StylelintBuilderOptions {
 	files: string[];
 }
 
-export default class StylelintBuilder implements Builder<StylelintBuilderOptions> {
+async function _run(
+	options: StylelintBuilderOptions,
+	context: BuilderContext
+): Promise<BuilderOutput> {
+	const systemRoot = context.workspaceRoot;
+	const { stylelintConfig, format: formatter, fix, force, silent } = options;
 
-	constructor(
-		public context: BuilderContext
-	) {
+	const config = stylelintConfig
+		? JSON.parse(readFileSync(path.resolve(systemRoot, stylelintConfig), "utf-8"))
+		: undefined;
+
+	console.log(getFilesToLint(systemRoot, options));
+	const result = await stylelint.lint({
+		config,
+		formatter,
+		configBasedir: systemRoot,
+		files: getFilesToLint(systemRoot, options),
+		fix
+	});
+
+	if (!silent) {
+		console.log(result.errored);
+		if (result.errored) {
+			context.logger.error("Lint errors found in the listed files.");
+		} else {
+			context.logger.info("All files pass linting.");
+		}
+
+		const errored = result.results.filter(x => x.errored);
+
+		if (errored.length) {
+			context.logger.info(stylelint.formatters.string(errored));
+		}
 	}
 
-	run(builderConfig: BuilderConfiguration<StylelintBuilderOptions>): Observable<BuildEvent> {
-		const root = this.context.workspace.root;
-		const systemRoot = getSystemPath(root);
-
-		const { stylelintConfig, format: formatter, fix, force, silent } = builderConfig.options;
-
-		const config = stylelintConfig
-			? JSON.parse(readFileSync(path.resolve(systemRoot, stylelintConfig), "utf-8"))
-			: undefined;
-
-		return from(
-			stylelint.lint({
-				config,
-				formatter,
-				configBasedir: systemRoot,
-				files: this.getFilesToLint(systemRoot, builderConfig.options),
-				fix
-			})
-		)
-			.pipe(
-				concatMap(result => new Observable(obs => {
-					if (!silent) {
-						if (result.errored) {
-							this.context.logger.error("Lint errors found in the listed files.");
-						} else {
-							this.context.logger.info("All files pass linting.");
-						}
-
-						const errored = result.results.filter(x => x.errored);
-
-						if (errored.length) {
-							this.context.logger.info(stylelint.formatters.string(errored));
-						}
-					}
-
-					const success = force || !result.errored;
-					obs.next({ success });
-
-					return obs.complete();
-				}))
-			);
-	}
-
-	getFilesToLint(root: string, options: StylelintBuilderOptions): string[] {
-		const { files, exclude: ignore } = options;
-
-		return files
-			.map(file => glob.sync(file, { cwd: root, ignore, nodir: true }))
-			.reduce((prev, curr) => prev.concat(curr), [])
-			.map(file => path.join(root, file));
-	}
+	return {
+		success: force || !result.errored
+	};
 }
+
+function getFilesToLint(root: string, options: StylelintBuilderOptions): string[] {
+	const { files, exclude: ignore } = options;
+
+	return files
+		.map(file => glob.sync(file, { cwd: root, ignore, nodir: true }))
+		.reduce((prev, curr) => prev.concat(curr), [])
+		.map(file => path.join(root, file));
+}
+
+export default createBuilder<StylelintBuilderOptions>(_run);
